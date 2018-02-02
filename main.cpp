37fafc52302272b3ad206da421001b45dc47891c
Emil <emil.ringgaard@hotmail.com>
2018-02-02 17:04:25 +0100

    added dummy targets

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int main()
+{
+    printf("hello from USV_autopilot!\n");
+	int x;
+	cin >> x;
+	cout << x << endl;
+	cout << "sumasdsdnde" << endl;
+    return 0;
+}
